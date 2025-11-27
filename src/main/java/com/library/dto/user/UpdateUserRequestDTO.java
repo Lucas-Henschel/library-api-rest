2536b603ce44239b2a618658a6fe51309df399ac
Lucas Gabriel Henschel. <lucas101henschel@gmail.com>
2025-11-27 12:49:56 -0300

    Add Size validation to UpdateUserRequestDTO

--- src/main/java/com/library/dto/user/UpdateUserRequestDTO.java
+++ src/main/java/com/library/dto/user/UpdateUserRequestDTO.java
@@ -1,6 +1,7 @@
 package com.library.dto.user;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
